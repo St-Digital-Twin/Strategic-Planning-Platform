fc6fe45cafa0eebbdf822d24af89c8740a4a64e6
Your Name <quasiperelman@gmail.com>
2022-12-23 19:38:06 +0300

    d

--- 3.Shiny/121.EurochemP/global.R
+++ 3.Shiny/121.EurochemP/global.R
@@ -2,6 +2,8 @@
 
 setwd('/mnt/seafiles.dtwin.ru/121.EurochemP')
 
+library(qs)
+
 source('5.R/2.EuroChemFunc.R', encoding = 'UTF-8')
 source('5.R/3.UIFunc.R', encoding = 'UTF-8')
 source('3.Shiny/121.EurochemP/visnetwork_fun.R', encoding = 'UTF-8')
